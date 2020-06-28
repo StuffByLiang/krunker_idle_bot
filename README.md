e5f53ec168776566455f58938e9eb19b41a99bfc
liang303 <stuffbyliang@gmail.com>
2020-06-28 04:04:48 -0700

    final version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # krunker_idle_bot
-Krunker idle bot utilizing threading and selenium to flood a server with bots
+Krunker.io idle bot utilizing threading and selenium to flood a server with bots
+
+# How to use
+download the latest geckodriver from https://github.com/mozilla/geckodriver/releases and the latest firefox release. Edit index.py to point to the path of the geckodriver executable.
+
+Install requirements
+```
+pip3 install -r requirements.txt
+```
+
+Finally, start
+```
+py index.py
+```
--- index.py
+++ index.py
@@ -4,32 +4,49 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 acceptTermsButton = '/html/body/div[2]/div[5]/div/div/div[2]/div[2]'
 gameCanvas = '/html/body/canvas[2]'
 instructions = '//*[@id="instructions" and contains(text(), "CLICK")]'
+path = r"C:\Users\lili3\Documents\python\geckodriver-v0.26.0-win64\geckodriver.exe"
 
 def thread_function(arg):
-  driver = webdriver.Firefox(executable_path="D:\Downloads\geckodriver-v0.26.0-win64\geckodriver.exe")
-  driver.get(url)
+  header = "Thread " + str(arg) + ": " 
+  opts = webdriver.FirefoxOptions()
+  # opts.add_argument("--headless")
+  opts.add_argument("--width=400")
+  opts.add_argument("--height=400")
+  driver = webdriver.Firefox(options=opts, executable_path=path)
+  driver.get("https://krunker.io/?game=SV:" + url)
 
-  element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, acceptTermsButton)))
-  element.click()
-  print("loaded = accepted terms")
+  send_space = ActionChains(driver).send_keys(Keys.SPACE)
 
   while True:
     try:
-      element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, instructions)))
-      driver.find_element(By.XPATH, gameCanvas).click()
-      print("clicked!")
+      element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, acceptTermsButton)))
+      element.click()
+      print(header + "accepted the terms & conditions")
+      break
     except TimeoutException:
-      print("timeout, retrying")
+      print(header + "retrying accepting terms & conditions")
+      # driver.save_screenshot('test.png')
+
+  while True:
+    try:
+      element = WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.XPATH, instructions)))
+      driver.find_element(By.XPATH, gameCanvas).click()
+      print(header + "clicked!")
+    except TimeoutException:
+      # in game
+      send_space.perform()
+      print(header + "sending space to prevent afk")
   driver.close()
   
-
-nThreads = int(input("No of threads\n"))
-url = input("Enter server url\n")
-for index in range(nThreads):
-    x = threading.Thread(target=thread_function, args=(0,))
-    x.start()
+if __name__ == "__main__":
+  nThreads = int(input("No of threads\n"))
+  url = input("Enter server code\n")
+  for index in range(nThreads):
+      x = threading.Thread(target=thread_function, args=(index,))
+      x.start()
