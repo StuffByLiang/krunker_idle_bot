4b767d44540fba25c2d2ef742c6bc4f814ae68b3
Liang Liu <stuffbyliang@gmail.com>
2020-06-29 03:23:32 -0700

    added headless firefox testing

--- index.py
+++ index.py
@@ -6,20 +6,40 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 acceptTermsButton = '/html/body/div[2]/div[5]/div/div/div[2]/div[2]'
 gameCanvas = '/html/body/canvas[2]'
 instructions = '//*[@id="instructions" and contains(text(), "CLICK")]'
-path = r"C:\Users\lili3\Documents\python\geckodriver-v0.26.0-win64\geckodriver.exe"
+path = r"D:\Downloads\geckodriver-v0.26.0-win64\geckodriver.exe"
+chromePath = r"D:\Downloads\chromedriver_win32\chromedriver.exe"
 
 def thread_function(arg):
   header = "Thread " + str(arg) + ": " 
   opts = webdriver.FirefoxOptions()
-  # opts.add_argument("--headless")
-  opts.add_argument("--width=400")
-  opts.add_argument("--height=400")
-  driver = webdriver.Firefox(options=opts, executable_path=path)
+  opts.add_argument("--headless")
+  d = DesiredCapabilities.FIREFOX
+  d['loggingPrefs'] = {'browser': 'ALL'}
+  # opts.add_argument("--width=400")
+  # opts.add_argument("--height=400")
+  driver = webdriver.Firefox(capabilities=d, options=opts, executable_path=path)
+
+  # chrome_options = Options()
+  # # chrome_options.add_argument("--headless")
+  # driver = webdriver.Chrome(options=chrome_options, executable_path = chromePath)
+
   driver.get("https://krunker.io/?game=SV:" + url)
+
+  print(driver.execute_script("""
+    console.error=function(msg){
+      window.errs.push(msg);
+    };
+    console.log("bitch");
+    window.errs = [];
+    return "lol"
+  """))
 
   send_space = ActionChains(driver).send_keys(Keys.SPACE)
 
@@ -28,20 +48,27 @@ def thread_function(arg):
       element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, acceptTermsButton)))
       element.click()
       print(header + "accepted the terms & conditions")
+      driver.save_screenshot('test.png')
       break
     except TimeoutException:
       print(header + "retrying accepting terms & conditions")
-      # driver.save_screenshot('test.png')
+      # for entry in driver.get_log('browser'):
+      #   print(entry)  
+      print (driver.execute_script("return window.errs;"))
+      driver.save_screenshot('test.png')
 
   while True:
     try:
       element = WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.XPATH, instructions)))
       driver.find_element(By.XPATH, gameCanvas).click()
+      # driver.switch_to.window(driver.current_window_handle)
       print(header + "clicked!")
+      driver.save_screenshot('test.png')
     except TimeoutException:
       # in game
       send_space.perform()
       print(header + "sending space to prevent afk")
+      driver.save_screenshot('test.png')
   driver.close()
   
 if __name__ == "__main__":
